50571dbe938581bcb86978414f02ab29df509170
Fabian <faboweber@gmail.com>
2017-01-24 14:16:27 +0100

    error on build with visualizer

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,14 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import jsx from 'rollup-plugin-jsx';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
+import visualizer from 'rollup-plugin-visualizer';
 
 export default {
     entry: './src/index.tsx',
     sourceMap: true,
 
     plugins: [
+        visualizer(),
         jsx( {factory: 'h'} ),
         nodeResolve({
             jsnext: true, 
